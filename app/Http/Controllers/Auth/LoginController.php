d5321ad2b58088002325cf4fe88990826b42fc15
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-07-24 13:19:51 +0700

    try to fix login issue

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,9 +38,6 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        if (Auth::check()){
-            $this->redirectTo();
-        }
     }
     
     /**
